eee72930d3504cd05855d0932cb0a49715b79f09
sasmitapanda02 <130721029+sasmitapanda02@users.noreply.github.com>
2023-07-07 15:05:28 +0530

    Dbconnection.java

--- src/main/java/prodevance/advance/databaseconnection/Dbconnection.java
+++ src/main/java/prodevance/advance/databaseconnection/Dbconnection.java
@@ -1,2 +1,38 @@
-package prodevance.advance.databaseconnection;public class Dbconnection {
+package prodevance.advance.databaseconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Dbconnection {
+    private static String driver="";
+    private static String url="";
+    private static String user="";
+    private static String password="";
+
+    private static Connection connection=null;
+
+    public static Connection getConnection(
+            String driver,
+            String url,
+            String user,
+            String password
+    ) {
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(
+                    url,
+                    user,
+                    password
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
 }
+
+
